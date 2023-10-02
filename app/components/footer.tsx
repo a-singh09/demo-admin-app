b10b0b6d389b32fea7049cc1aef505a884e23749
a-singh09 <a_singh09@yahoo.com>
2023-10-02 21:43:16 +0530

    [Fix] Fixed dashboard UI

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -10,9 +10,7 @@ function Footer() {
                         <a href="http://www.sturec.in/" target="_blank" className="font-medium text-rose-600 hover:text-rose-400 dark:text-rose-400 dark:hover:text-rose-300">Sturec</a> ©
                     </div>
                     <div className="pb-4 pt-1 md:pt-4 inline-flex items-center justify-center">
-                        <span>Crafted with</span>
-                        <svg className="hi-solid hi-heart inline-block w-4 h-4 mx-1 text-red-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" /></svg>
-                        <span>by <a href="https://github.com/a-singh09" target="_blank" className="font-medium text-rose-600 hover:text-rose-400 dark:text-rose-400 dark:hover:text-rose-300">Anshdeep Singh</a></span>
+                        <span>Crafted by <a href="https://github.com/a-singh09" target="_blank" className="font-medium text-rose-600 hover:text-rose-400 dark:text-rose-400 dark:hover:text-rose-300">Anshdeep Singh</a></span>
                     </div>
                 </div>
             </footer>
