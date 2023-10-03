875c5df5668fe0a8ae9b5fab0fc45e7a4904ec94
a-singh09 <a_singh09@yahoo.com>
2023-10-03 10:12:36 +0530

    [Feat] Added events page

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -3,7 +3,7 @@
 import DropdownButton from "./dropdownButton";
 
 interface User {
-    name: string;
+    name?: string;
     admissionNumber?: string;
     rollNumber?: number;
     fees?: number;
@@ -22,6 +22,11 @@ interface User {
     hindi?: number;
     totalMarks?: number;
     percentage?: number;
+
+    // events
+    eventName?: string;
+    fromDate?: string;
+    toDate?: string;
 }
 
 interface DropdownItem {
@@ -47,25 +52,31 @@ const Table: React.FC<TableProps> = ({ users, dropdownItems }) => {
                     key={index}
                     className="border-b border-gray-100 dark:border-gray-700/50"
                 >
-                    <td className="pr-3 py-3 font-medium">{user.name}</td>
-                    <td className="p-3 text-gray-500 dark:text-gray-400">
+                    {user.name && <td className="pr-3 py-3 font-medium">{user.name}</td>}
+                    {user.admissionNumber && <td className="p-3 text-gray-500 dark:text-gray-400">
                         {user.admissionNumber}
-                    </td>
-                    {user.fees && <td className="p-3 text-right">Rs. {user.fees}</td>}
-                    {user.balance && <td className="p-3 text-right">Rs. {user.balance}</td>}
-                    {user.class && <td className="p-3 text-right">{user.class}</td>}
-                    {user.section && <td className="p-3 text-right">{user.section}</td>}
-                    {user.contact && <td className="p-3 text-right">{user.contact}</td>}
-                    {user.rollNumber && <td className="p-3 text-right">{user.rollNumber}</td>}
-                    {user.attendance && <td className="p-3 text-right">{user.attendance}</td>}
-                    {user.english && <td className="p-3 text-right">{user.english}</td>}
-                    {user.punjabi && <td className="p-3 text-right">{user.punjabi}</td>}
-                    {user.maths && <td className="p-3 text-right">{user.maths}</td>}
-                    {user.science && <td className="p-3 text-right">{user.science}</td>}
-                    {user.social && <td className="p-3 text-right">{user.social}</td>}
-                    {user.hindi && <td className="p-3 text-right">{user.hindi}</td>}
-                    {user.totalMarks && <td className="p-3 text-right">{user.totalMarks}</td>}
-                    {user.percentage && <td className="p-3 text-right">{user.percentage}</td>}
+                    </td>}
+                    {user.fees && <td className="p-3 text-center">Rs. {user.fees}</td>}
+                    {user.balance && <td className="p-3 text-center">Rs. {user.balance}</td>}
+                    {user.class && <td className="p-3 text-center">{user.class}</td>}
+                    {user.section && <td className="p-3 text-center">{user.section}</td>}
+                    {user.contact && <td className="p-3 text-center">{user.contact}</td>}
+                    {user.rollNumber && <td className="p-3 text-center">{user.rollNumber}</td>}
+                    {user.attendance && <td className="p-3 text-center">{user.attendance}</td>}
+                    {user.english && <td className="p-3 text-center">{user.english}</td>}
+                    {user.punjabi && <td className="p-3 text-center">{user.punjabi}</td>}
+                    {user.maths && <td className="p-3 text-center">{user.maths}</td>}
+                    {user.science && <td className="p-3 text-center">{user.science}</td>}
+                    {user.social && <td className="p-3 text-center">{user.social}</td>}
+                    {user.hindi && <td className="p-3 text-center">{user.hindi}</td>}
+                    {user.totalMarks && <td className="p-3 text-center">{user.totalMarks}</td>}
+                    {user.percentage && <td className="p-3 text-center">{user.percentage}</td>}
+
+                    {/* events */}
+                    {user.eventName && <td className="p-3 text-center">{user.eventName}</td>}
+                    {user.fromDate && <td className="p-3 text-center">{user.fromDate}</td>}
+                    {user.toDate && <td className="p-3 text-center">{user.toDate}</td>}
+
                     <td className="pl-3 py-3 text-right">
                         <DropdownButton items={dropdownItems} />
                     </td>
